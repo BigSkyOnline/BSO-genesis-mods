7cb18c2385411d787d6c645e9163d16d675ff8aa
Bradford Knowlton <brad@ucanbonline.net>
2014-07-14 12:48:25 -0600

    Added custom header

--- bso-genesis-mods.php
+++ bso-genesis-mods.php
@@ -1,11 +1,10 @@
 <?php
 
-
 /*
 Plugin Name: BSO Genesis Mods
 Plugin URI: https://github.com/BigSkyOnline/BSO-genesis-mods/
-Description: Add additional fields to user profiles for viewing and editing on backend of WordPress.
-Version: 1.7.8
+Description: Add additional customizations to Genesis Theme.
+Version: 1.7.9
 Author: Bradford Knowlton
 Author URI: http://bradknowlton.com/
 License:           GNU General Public License v2
@@ -17,3 +16,17 @@ GitHub Branch:     bevxusa.com
 */
 
 
+remove_action( 'genesis_header', 'genesis_do_header' ); 
+add_action( 'genesis_header', 'genesis_do_new_header' ); 
+function genesis_do_new_header() { 
+    echo '<div id="title-area"><img src="your/logo/image.jpg" alt="Site Logo" />'; 
+    do_action( 'genesis_site_title' ); 
+    do_action( 'genesis_site_description' ); 
+    echo '</div><!-- end #title-area -->'; 
+    if ( is_active_sidebar( 'header-right' ) || has_action( 'genesis_header_right' ) ) { 
+        echo '<div class="widget-area">'; 
+        do_action( 'genesis_header_right' ); 
+        dynamic_sidebar( 'header-right' ); 
+        echo '</div><!-- end .widget-area -->'; 
+    } 
+} 
