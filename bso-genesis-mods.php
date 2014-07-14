5f7226900cdb7f1537bf6319f19d22f5cab17318
Bradford Knowlton <brad@ucanbonline.net>
2014-07-14 12:44:23 -0600

    Initial Commit

---
+++ bso-genesis-mods.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/*
+Plugin Name: BSO Genesis Mods
+Plugin URI: https://github.com/BigSkyOnline/BSO-genesis-mods/
+Description: Add additional fields to user profiles for viewing and editing on backend of WordPress.
+Version: 1.7.8
+Author: Bradford Knowlton
+Author URI: http://bradknowlton.com/
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+Domain Path:       /languages
+Text Domain:       BSO-genesis-mods
+GitHub Plugin URI: https://github.com/BigSkyOnline/BSO-genesis-mods
+GitHub Branch:     bevxusa.com
+*/
+
+
